01647c6ec055e2bc1d57b2d002e16eaca86497fd
uty <fgmapam@gmail.com>
2025-11-07 20:42:55 +0300

    Summary(required)

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,19 +1,18 @@
 import { Router } from 'express';
-import { registerUserSchema } from '../validation/auth.js';
-import { userRegisterController } from '../controllers/auth.js';
+import { registerUserSchema,loginUserSchema } from '../validation/auth.js';
+import { userRegisterController, 
+  userLoginController
+ } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
 
 const router = Router();
 
-router.post('/login', (req, res) => {
-  return res.status(200).json({
-    message: 'Login endpoint works!',
-  });
-});
+
 router.post(
   '/register',
   validateBody(registerUserSchema),
   userRegisterController,
 );
+router.post('/login', validateBody(loginUserSchema), userLoginController);
 
 export const authRouter = router;
