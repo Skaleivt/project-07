0f664d2df82073d8d574e124257bd88098b8ffa4
Yuliia <chernyshevaulia2905@gmail.com>
2025-11-07 01:10:30 -0700

    add public user registration

---
+++ src/routers/auth.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { registerUserSchema } from '../validation/auth.js';
+import { userRegisterController } from '../controllers/auth.js';
+import { validateBody } from '../middlewares/validateBody.js';
+
+const router = Router();
+
+router.post(
+  '/register',
+  validateBody(registerUserSchema),
+  userRegisterController,
+);
+
+export default router;
