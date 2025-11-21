25d6ff074a5160d2d33c25fb33502ab618f810fc
Skaleivt <skaleiviktor@gmail.com>
2025-11-21 13:32:47 +0200

    fix2

--- src/server.js
+++ src/server.js
@@ -16,14 +16,9 @@ export const setupServer = () => {
   const app = express();
   const PORT = Number(process.env.PORT) || 3000;
 
-  // app.use(cors());
   app.use(
     cors({
-      origin: [
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'http://localhost:3002',
-      ],
+      origin: '*',
       credentials: true,
     }),
   );
